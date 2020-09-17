eefd43184cdea9fbe0d2c1d7f022b0354f1f39a8
KalebJohnson <Kaleb.Johnson45@yahoo.com>
2020-09-16 20:37:04 -0500

    setup

---
+++ users-auth/auth-model.js
@@ -0,0 +1,25 @@
+const db = require("../database/config")
+
+async function addUser(user) {
+    const [id] = await db("users").insert(user)
+    return findById(id)
+}
+
+function findBy(filter) {
+    return db("users")
+        .select("id", "username", "password", "phoneNumber")
+        .where(filter)
+}
+
+function findById(id) {
+    return db("users")
+        .select("id", "username", "phoneNumber")
+        .where({ id })
+        .first()
+}
+
+module.exports = {
+    addUser,
+    findBy,
+    findById
+}
---
+++ users-auth/auth-router.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const Users = require("./auth-model")
+
+router.post('/register', async (req, res, next) => {
+	try {
+		const { username, password, phoneNumber} = req.body
+		const user = await Users.findBy({ username }).first()
+
+		if (user) {
+			return res.status(409).json({
+				message: "Username is already taken",
+			})
+		}
+
+		const newUser = await Users.addUser({
+			username,
+            password: await bcrypt.hash(password, 8),
+            phoneNumber,
+			
+		})
+
+		res.status(201).json({message: `Welcome, ${newUser.username}!`})
+	} catch(err) {
+		next(err)
+	}
+});
+
+
+router.post('/login', async (req, res, next) => {
+	try {
+		const { username, password, id } = req.body
+		const user = await Users.findBy({ username }).first()
+		
+		if (!user) {
+			return res.status(401).json({
+				message: "Invalid Credentials",
+			})
+		}
+
+
+		const passwordValid = await bcrypt.compare(password, user.password)
+
+
+		if (!passwordValid) {
+			return res.status(401).json({
+				message: "Invalid Credentials",
+			})
+		}
+
+
+		const token = jwt.sign({
+			userID: user.id,
+    }, process.env.SECRET)
+
+
+
+		res.status(200).json({
+			message: `Hey, ${user.username} come here often?`,
+			token,
+			userid: user.id
+		})
+	} catch(err) {
+		next(err)
+	}
+});
+
+
+
+
+module.exports = router;
