efe852e42e9a8432cab7b58f822f11acbfe47c53
KalebJohnson <Kaleb.Johnson45@yahoo.com>
2020-09-17 18:32:27 -0500

    removed zombie code, and formatted.

--- knexfile.js
+++ knexfile.js
@@ -1,40 +1,39 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: './database/data.db3',
+      filename: "./database/data.db3",
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done)
+        conn.run("PRAGMA foreign_keys = ON", done);
       },
     },
     migrations: {
-      directory: './database/migrations',
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: './database/seeds',
+      directory: "./database/seeds",
     },
   },
   // For testing later
   testing: {
-      client: 'sqlite3',
-      connection: {
-        filename: './database/test.db3',
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: './database/migrations',
-      },
-      seeds: {
-        directory: './database/seeds',
-      },
-      pool: {
-        afterCreate: (conn, done) => {
-          conn.run('PRAGMA foreign_keys = ON', done)
-        },
+    client: "sqlite3",
+    connection: {
+      filename: "./database/test.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
       },
     },
-  }
-  
+  },
+};
