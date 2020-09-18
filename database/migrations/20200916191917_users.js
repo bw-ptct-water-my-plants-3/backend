7ffec84a1765d61272c6766fee16eb9ea7318af0
William Sedlacek <wsedlacekc@gmail.com>
2020-09-17 17:02:26 -0700

    style: fix format linting
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- database/migrations/20200916191917_users.js
+++ database/migrations/20200916191917_users.js
@@ -1,15 +1,12 @@
 exports.up = async function (knex) {
-    await knex.schema
-    .createTable("users", (tbl) => {
-        tbl.increments("id").unsigned().primary()
-        tbl.text("username", 20).unique().notNullable()
-        tbl.text("password").notNullable();
-        tbl.text("phoneNumber").notNullable().unique()
-    })
-
-}
+  await knex.schema.createTable("users", (tbl) => {
+    tbl.increments("id").unsigned().primary();
+    tbl.text("username", 20).unique().notNullable();
+    tbl.text("password").notNullable();
+    tbl.text("phoneNumber").notNullable().unique();
+  });
+};
 
 exports.down = async function (knex) {
-    return knex.schema
-    .dropTableIfExists("users")   
-}// make sure to add these "backwards/reverse"
+  return knex.schema.dropTableIfExists("users");
+}; // make sure to add these "backwards/reverse"
