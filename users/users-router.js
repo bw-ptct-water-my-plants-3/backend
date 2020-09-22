56a44e0a087018b984cbafe7ba101d6cf2d11e5d
William Sedlacek <wsedlacekc@gmail.com>
2020-09-22 16:48:34 -0700

    lint fixes
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- users/users-router.js
+++ users/users-router.js
@@ -4,7 +4,6 @@ const router = require("express").Router();
 const users = require("./users-model");
 const plantsRouter = require("../plants/plants-router");
 
-
 router.get("/", async (req, res, next) => {
   try {
     res.json(await users.find());
