d1207994b81ba0deae50e0d03f957fcb4818afd7
KalebJohnson <Kaleb.Johnson45@yahoo.com>
2020-09-24 14:49:05 -0500

    Users end point/s tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -90,3 +90,62 @@ describe("register",  () => {
             expect(res.statusCode).toBe(409)
     })
 })
+
+ // without Auth middleware
+ //---------GET USERS--------------
+ describe("get users", () => {
+    it("Should return Users tbl with a .length >= 3 (seed array length)", async () => {
+        const res = await request(server)
+        .get("/users")
+        expect(res.statusCode).toBe(200)
+		expect(res.type).toBe("application/json")
+		expect(res.body.length).toBeGreaterThanOrEqual(3)
+        })
+    
+    })
+
+// -------GET USER BY :ID----------
+describe("get user by :id", () => {
+    it("should specific Json data (username) of users/1", async () => {
+        const res = await request(server)
+        .get("/users/1")
+        expect(res.statusCode).toBe(200)
+		expect(res.type).toBe("application/json")
+        expect(res.body.username).toBe("test1")
+        expect(res.body.phoneNumber).toBe("867-5309")
+    })
+    it("Shouldn't allow the hashed password to be returned (resulting in undefined)", async () =>{
+        const res = await request(server)
+        .get("/users/1")
+        expect(res.body.password).toBe(undefined)
+        expect(res.statusCode).toBe(200)
+    })
+    it("Should return status 404 if user doesn't exist", async () =>{
+        const res = await request(server)
+        .get("/users/10")
+        expect(res.statusCode).toBe(404)
+    })   
+})
+
+// ----------- PUT/EDIT USER ----------
+describe("put/edit user info", () => {
+    it("Returns status 400 due to not providing a phoneNumber", async () => {
+        const res = await request(server)
+        .put("/users/1")
+        .send({
+            password:"password"
+        })
+        expect(res.statusCode).toEqual(400)
+        })
+    it("Returns status 404 body is sent, but the user is not found", async () => {
+        const res = await request(server)
+        .put("/users/10")
+        .send({
+            password:"password",
+            phoneNumber:"12345"
+        })
+        expect(res.statusCode).toEqual(404)
+        })
+    })    
+    
+
