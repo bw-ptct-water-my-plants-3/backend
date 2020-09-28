9066536e170fa7003a2ef8f1c96efc0b9765ee9d
William Sedlacek <wsedlacekc@gmail.com>
2020-09-27 17:02:38 -0700

    fix: actually fix cors
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = express();
 require("dotenv/config");
 
 server.use(helmet());
-server.use(cors({ credentials: true, origin: ["http://localhost:3000"] }));
+server.use(cors({ origin: ["http://localhost:3000"] }));
 server.use(express.json());
 server.use(express.urlencoded());
 server.use(
@@ -17,15 +17,6 @@ const restrict = require("./middleware/authenticate");
 const authRouter = require("./users-auth/auth-router.js");
 const usersRouter = require("./users/users-router");
 const welcomeRouter = require("./welcome");
-
-// do NOT put "/" at the end of an address or it will break CORS origin policy
-
-server.use(
-  cors({
-    origin: "http://localhost:5000",
-    credentials: true,
-  })
-);
 
 server.use("/users/", restrict(), usersRouter);
 server.use("/auth", authRouter);
